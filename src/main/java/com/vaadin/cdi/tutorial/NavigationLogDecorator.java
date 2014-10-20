18e9782ada7862e6a4e3efa1164b20d462d91277
Juuso Valli <juuso@vaadin.com>
2014-10-20 11:29:08 +0300

    Add a logging decorator to the NavigationService

---
+++ src/main/java/com/vaadin/cdi/tutorial/NavigationLogDecorator.java
@@ -0,0 +1,33 @@
+package com.vaadin.cdi.tutorial;
+
+import java.util.logging.Logger;
+
+import javax.decorator.Decorator;
+import javax.decorator.Delegate;
+import javax.enterprise.event.Observes;
+import javax.enterprise.inject.Any;
+import javax.inject.Inject;
+
+@Decorator
+public class NavigationLogDecorator implements NavigationService {
+
+    @Inject
+    @Delegate
+    @Any
+    NavigationService delegate;
+
+    @Inject
+    private UserInfo userInfo;
+
+    @Override
+    public void onNavigationEvent(@Observes NavigationEvent event) {
+        getLogger().info(
+                userInfo.getName() + " navigated to " + event.getNavigateTo());
+        delegate.onNavigationEvent(event);
+    }
+
+    private Logger getLogger() {
+        return Logger.getLogger(this.getClass().getSimpleName());
+    }
+
+}
